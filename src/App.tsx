639185f3d6b14c8f751f4a53ca897c035087c954
Polina Lozova <1polinalozovaya1@gmail.com>
2024-10-03 19:55:14 +0300

    passing code into iframe

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {unpkgPathPlugin} from "./plugins/unpkg-path-plugin";
 import {fetchPlugin} from "./plugins/fetch-plugin";
 
 const App = () => {
-
+	const iframeRef = useRef<any>()
 	const [input, setInput] = useState('');
 	const [code, setCode] = useState('');
 
@@ -25,7 +25,9 @@ const App = () => {
 				}
 			});
 
-			setCode(res.outputFiles[0].text)
+			// setCode(res.outputFiles[0].text)
+
+			iframeRef.current.contentWindow.postMessage(res.outputFiles[0].text, '*')
 		}catch(err){
 			console.error(err);
 		}
@@ -35,26 +37,44 @@ const App = () => {
 		try {
 			await esbuild.initialize({
 				worker: true,
-				wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
-			});
+				wasmURL: 'https://unpkg.com/esbuild-wasm@latest/esbuild.wasm'
+		});
 			esbuildInitializedRef.current = true;
+			console.log("esbuild initialized successfully");
 		} catch(err) {
 			esbuildInitializedRef.current = false;
-			console.log(err)
+			console.error(err)
 		}
 	}, []);
 
 	useEffect(()=> {
 		startService();
-	}, [])
+	}, [startService]);
+
+
+	const html = `
+		<html>
+			<head></head>
+			<body>
+				<div id="user-root"></div>
+				<script>
+					window.addEventListener('message', (e) => {
+						eval(e.data);
+					},false);
+				</script>
+			</body>
+		</html>	
+	`;
 
 	return (
+
 		<div>
 			<textarea value={input} onChange={(e:ChangeEvent<HTMLTextAreaElement>) => setInput(e.target.value)}></textarea>
 			<div>
 				<button onClick={onClickHandler}>Submit</button>
 			</div>
 			<pre>{code}</pre>
+			<iframe ref={iframeRef} title='users-code' srcDoc={html} sandbox='allow-scripts'/>
 		</div>
 	);
 };
